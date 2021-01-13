3ab8f47241122ee4b37beb744fbf471e4bfdaad3
abdelrahmansaf <77361942+abdelrahmansaf@users.noreply.github.com>
2021-01-13 11:44:44 +0100

    Update Installing_Discoord.sh

--- Installing_Discoord.sh
+++ Installing_Discoord.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 echo "UBUNTU POST-INSTALL SCRIPT"
+echo "Updating APT..."
+#installing base package
+sudo apt-get update
+#sudo apt-get update
 clear
-echo "Installing base packages"
-sudo apt-get install --yes git git-extras python3-pip
-echo "Installing Discord"
-sudo snap install discord
-echo "Installing Visual studio code "
-sudo snap install code
+echo"Installing base packages"
+sudo aptget install --yes git git-extras build-essentials python3-pip htop glances
+echo"installing discord"
+echo "sudo snap install discord"
+snap refresh
+echo "install code --classic"
+sudo snap install code --classic
