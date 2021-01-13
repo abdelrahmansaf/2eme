2d0406b20eadf43d767100a589d2f8a07748e49f
abdelrahmansaf <77361942+abdelrahmansaf@users.noreply.github.com>
2021-01-13 11:42:36 +0100

    Update Installing_Discoord.py

--- Installing_Discoord.py
+++ Installing_Discoord.py
@@ -1,14 +1,14 @@
 import os
 print("UBUNTU POST-INSTALL SCRIPT")
-#update the system
-os.system("sudo apt-get update")
+print("Updating APT...")
 #installing base package
-os.system("sudo apt-get install --yes git git-extras build-essential python3-pip htop glances")
-print("dowloading discord")
-os.system("wget -O discord.deb 'https://discordapp.com/api/download?platform=linux&format=deb'")
+os.system("sudo apt-get update")
+#sudo apt-get update
+os.system ("clear")
+print("Installing base packages")
+os.system ("sudo aptget install --yes git git-extras build-essentials python3-pip htop glances")
 print("installing discord")
-os.system("sudo dpkg -i /path/to/discord.deb")
+os.system ("sudo snap install discord")
 os.system("snap refresh")
-os.system("sudo snap install code --classic")
-print("python3 installed")
-print("Discord installed")
+print("install code --classic")
+os.system ("sudo snap install code --classic")
