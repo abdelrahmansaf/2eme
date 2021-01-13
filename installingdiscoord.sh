daeca1cb5133ba5c9692d0027599b275c062c8cd
abdelrahmansaf <77361942+abdelrahmansaf@users.noreply.github.com>
2021-01-13 10:18:51 +0100

    Update and rename first.sh to installingdiscoord.sh

--- installingdiscoord.sh
+++ installingdiscoord.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo "UBUNTU POST-INSTALL SCRIPT"
-echo "installing discord"
+#update the system
 sudo apt-get update
+#installing base package
 sudo apt-get install --yes git git-extras build-essential python3-pip htop glances
+echo "dowloading discord"
 wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+echo "installing discord"
 sudo dpkg -i /path/to/discord.deb
 snap refresh
 sudo snap install code --classic
